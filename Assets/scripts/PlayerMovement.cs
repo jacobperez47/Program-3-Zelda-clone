f1a6261340d32b564fa0d59c5ca756b55d82284c
jacobperez47 <77548177+jacobperez47@users.noreply.github.com>
2025-10-05 16:00:48 -0700

    added movement animations

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -8,22 +8,34 @@ public class NewBehaviourScript : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
-    
+    private Animator animator;
+
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        UpdateCharacterAnimation();
+    }
+
+    private void UpdateCharacterAnimation()
+    {
         if (change != Vector3.zero)
         {
             MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
         }
+        else
+            animator.SetBool("moving", false);
     }
 
     void MoveCharacter()
