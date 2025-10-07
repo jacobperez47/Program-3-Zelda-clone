a1596088f1f89582cf893a056d92d1a82ec65290
jacobperez47 <77548177+jacobperez47@users.noreply.github.com>
2025-10-06 22:42:57 -0700

    continued working on log script

--- Assets/scripts/Log Script.cs
+++ Assets/scripts/Log Script.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LogScript : MonoBehaviour
+public class LogScript : EnemyScript
+
+
+
 {
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
