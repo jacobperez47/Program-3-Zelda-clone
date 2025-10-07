db78b71923be75bd4b2896374eced376770093b4
jacobperez47 <77548177+jacobperez47@users.noreply.github.com>
2025-10-06 23:36:10 -0700

    added enemy ai that moves to the player

--- Assets/scripts/Log Script.cs
+++ Assets/scripts/Log Script.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,80 @@ using UnityEngine;
 public class LogScript : EnemyScript
 
 
-
 {
-    public Transform target;
+    private Transform target;
+
+
+    public float speed;
+
+    public int baseDamage;
+
+    public CircleCollider2D vision;
+
+    public CircleCollider2D attack;
+
+    private bool isVisible;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        isVisible = false;
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            target = playerObject.transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void FixedUpdate()
+    {
+        if (isVisible && target && rb)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+
+            rb.MovePosition(transform.position + direction * (speed * Time.fixedDeltaTime));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (target == null)
+            {
+                target = other.transform;
+            }
+
+            if (other.IsTouching(vision))
+            {
+                isVisible = true;
+                Debug.Log("Player in vision range");
+            }
+
+            if (GetComponent<CircleCollider2D>() == attack)
+            {
+                Debug.Log("Player in attack range");
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (vision != null && !other.IsTouching(vision))
+            {
+                isVisible = false;
+            }
+
+            Debug.Log("Player out of vision range");
+        }
     }
 }
