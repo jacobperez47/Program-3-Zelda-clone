32245331a91ea31247dc5e9736d6db184a6a01f7
That0neGuy1 <163476246+That0neGuy1@users.noreply.github.com>
2025-10-07 23:21:38 -0700

    Sign finished, chest WIP

--- Assets/scripts/Plyaer Hit.cs
+++ Assets/scripts/Plyaer Hit.cs
@@ -17,7 +17,7 @@ public class PlayerHit : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("breakable")) ;
+        if (other.CompareTag("breakable"))
         {
             other.GetComponent<PotScript>().smash();
         }
---
+++ Assets/scripts/TreasureChest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour
+{
+    //Look for Interactable script in videos to inherit stuff
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
