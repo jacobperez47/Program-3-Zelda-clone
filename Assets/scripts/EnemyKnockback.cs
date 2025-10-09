0275736c32ef39259b275fb6b9cc2c0880fedaf9
jacobperez47 <77548177+jacobperez47@users.noreply.github.com>
2025-10-08 21:35:13 -0700

    implemented player knockback

--- Assets/scripts/EnemyKnockback.cs
+++ Assets/scripts/EnemyKnockback.cs
@@ -7,11 +7,16 @@ public class EnemyKnockback : MonoBehaviour
     public float thrust;
 
     private EnemyScript enemy;
+    private Rigidbody2D enemyRigidbody;
     // Start is called before the first frame update
     void Start()
     {
 
         enemy = GetComponentInParent<EnemyScript>();
+        if (enemy != null)
+        {
+            enemyRigidbody = enemy.GetComponent<Rigidbody2D>();
+        }
 
     }
 
@@ -33,13 +38,16 @@ public class EnemyKnockback : MonoBehaviour
 
             if (hit != null && enemy != null && player != null)
             {
+               
+                    enemyRigidbody.velocity = Vector2.zero;
+                    enemyRigidbody.angularVelocity = 0f;
                 if (player.currentState != PlayerStates.stagger && hit.IsTouching(enemy.attackHitboxes))
                 {
                     Vector2 difference = player.transform.position - transform.position;
                     Vector2 force = difference.normalized * thrust;
                     Debug.Log("Player knocked back");
                     player.currentState = PlayerStates.stagger;
-                    player.Knock(force, .2f);
+                    player.Knock(force, .25f);
                 }
             }
         }
